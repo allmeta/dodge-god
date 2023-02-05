86e60e59fd634efaa0b993a7d7728ee7dea3cd31
skykanin <3789764+skykanin@users.noreply.github.com>
2023-02-05 20:45:01 +0100

    rework 'handle' function

--- lib/Server.hs
+++ lib/Server.hs
@@ -1,12 +1,13 @@
 module Server (startServer) where
 
-import Data.Text.Lazy (Text, pack)
+import Data.ByteString.Lazy (ByteString)
+import Data.Text.Lazy (Text)
+import Data.Text.Lazy qualified as T
 import Data.Text.Lazy.Encoding (decodeUtf8, encodeUtf8)
 import Data.Word
 import Decrypt
-import Network.HTTP.Types (status200, status400)
+import Network.HTTP.Types (status400)
 import Web.Scotty
-import Web.Scotty.Internal.Types (ActionT)
 
 startServer :: Int -> Word8 -> IO ()
 startServer port key =
@@ -16,20 +17,18 @@ startServer port key =
       html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
     post "/" $ do
       p <- params
-      handle p key
+      res <- handle p key
+      text $ decodeUtf8 res
 
-handle :: [Param] -> Word8 -> ActionT Text IO ()
+handle :: [Param] -> Word8 -> ActionM ByteString
 handle p key =
   case lookup "s" p of
     Just r -> parseSubmission r key
     _ -> raiseStatus status400 "submission error"
 
-parseSubmission :: Text -> Word8 -> ActionT Text IO ()
-parseSubmission r key = do
-  let d = decrypt key $ encodeUtf8 r
-  case d of
-    Left e -> raiseStatus status400 $ pack $ show e
-    Right d' -> raiseStatus status200 $ decodeUtf8 d'
-
--- liftIO . print . decode @ScoreSubmission .
---
+parseSubmission :: Text -> Word8 -> ActionM ByteString
+parseSubmission r key =
+  either
+    (raiseStatus status400 . T.pack . show)
+    pure
+    (decrypt key $ encodeUtf8 r)
